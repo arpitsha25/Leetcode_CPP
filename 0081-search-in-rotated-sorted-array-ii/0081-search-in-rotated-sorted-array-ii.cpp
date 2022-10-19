35a64c32f5acd98eb47b32ba9328300e6ce4f804
arpitsha25 <102916927+arpitsha25@users.noreply.github.com>
2022-10-19 20:26:57 +0530

    Time: 24 ms (5.35%), Space: 14.1 MB (42.24%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+         int l = 0;
+        int r = nums.size() - 1;
         
-        while(left <= right)
+        while(l <= r)
         {
-            if(nums[left] == target || nums[right] == target)
-            {
+            int mid = l + (r-l) / 2;
+            if (nums[mid] == target)
                 return true;
+			// with duplicates we can have this contdition, just update left & right
+            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
             }
+			// first half
+			// first half is in order
+            else if(nums[l] <= nums[mid])
+            {
+				// target is in first  half
+                if((nums[l] <= target) && (nums[mid] > target))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+			// second half
+			// second half is order
+			// target is in second half
             else
             {
-                left ++;
-                right --;
+                if((nums[mid] < target) && (nums[r]>= target))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
         }
         return false;
     }
+    
 };
